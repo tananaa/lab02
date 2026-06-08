500d662d93a197426d184bfb01f135914e39c857
tananaa <tanyaknz07@gmail.com>
2026-06-08 18:02:37 +0300

    Apply Mozilla code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
+
 int main() {
-    // Important: prompt user for name.
+    
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    // Important: print personalized greeting.
+    
+int
+main()
+{
+    
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
